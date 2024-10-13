6eb3438c1cd3dcc8b628ea9472248f4dcaab9300
nangdypanhar <nangdypanhar@gmail.com>
2024-10-13 19:02:09 +0700

    finised all the assignment exercise

--- W1-S2-PRACTICE-Dart/EX-1.dart
+++ W1-S2-PRACTICE-Dart/EX-1.dart
@@ -1,1 +1,11 @@
 // Answer on doc file
+
+//  1. Map<String, list<int>> 
+//  2. Map<String, double> 
+//  3. List<Map<String, String>> 
+//  4. Map<String, Map< String,int>> 
+//  5. List<List<int>> 
+//  6. Map<String, dynamic> 
+//  7. List<int function(int,int)> 
+//  8. Set<double>
+//  9. Map<String , List<Map<String,Object>>> 
--- W1-S2-PRACTICE-Dart/EX-2.dart
+++ W1-S2-PRACTICE-Dart/EX-2.dart
@@ -2,27 +2,39 @@ void main() {
   // 1 - iLike
   String iLike = 'I like pizza';
 
+ // Answer : I choose CONST because we know exact value at the complie-time.
+
   // 2 - toppings
   String toppings = 'with tomatoes';
   toppings += " and cheese";
+
+  // Anwser : I choose VAR because the value of the variable can reassign.
 
   // 3 - message
   String message = '$iLike $toppings';
   print(message);
 
+  // Answer : I choose FINAL because the value of the variable is assigned at the run-time.
+
   // 4 - rbgColors
   List<String> rbgColors = ['red', 'blue', 'green'];
   print(rbgColors);
+
+  // Answer : I choose CONST because the variable cannot change.
 
   // 5 - weekDays
   List<String> weekDays = ['monday', 'tuesday', 'wednesday'];
   weekDays.add('thursday');
   print(weekDays);
 
+  // Answer : I choose FINAL because the variable cannot reassign but the content of the list can be modify.
+
   // 6 - scores
   List<int> scores = [45,35,50];
   scores = [45,35,50, 78];
   print(scores);
+
+  // Answer : I choose VAR because the variable can reassign the value.
 }
 
 
